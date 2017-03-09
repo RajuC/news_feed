47bd6a6e7fe505bdc7a3307d8374bc3c18d03fee
RajuC <raju.chittimallea@gmail.com>
2017-03-09 15:40:02 -0800

    add the sparkpost and news_feed to config file

--- config/config.exs
+++ config/config.exs
@@ -31,3 +31,8 @@ config :phoenix, :generators,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+
+config :sparkpost, api_key: "sdfdgdfgdfgdf"
+
+config :news_feed, :domain, "raju.dfgdf@dfgdfdfgdf.com"
